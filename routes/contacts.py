fa93124d2dd1678bdd9f1ed3fd5f8eb9b9fa4065
achucarrob <achucarroclaudia@gmail.com>
2023-04-27 20:11:34 -0400

    CRUD its fishish

--- routes/contacts.py
+++ routes/contacts.py
@@ -1,5 +1,5 @@
 # importar Blueprint
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, url_for, redirect
 
 # importar la base de datos para traer los datos cuando se complete el form
 from models.contact import Contact # Import el modelo de base de datos
@@ -11,34 +11,42 @@ contacts = Blueprint("contacts", __name__)
 # En vez de referirnos a app nos referimos a contacts
 
 @contacts.route("/")
-def libreta():
+def index():
     mi_libreta = Contact.query.all()
     return render_template('index.html', libreta=mi_libreta)
 
 # Trae los datos desde el form que esta en index.html
 @contacts.route("/create", methods=['POST'])
 def nuevo_contacto():
-    new_fullname = request.form['fullname']
-    new_phone = request.form['phone']
+    fullname = request.form['fullname']
+    phone = request.form['phone']
 
 # new_contact devuelve un obj
-    new_contact = Contact(new_fullname, new_phone)
+    new_contact = Contact(fullname, phone)
 
 # para guardar en la base de datos
     db.session.add(new_contact)
     db.session.commit()
 
-    return 'guarde un contacto'
+    return redirect(url_for('contacts.index'))
 
-@contacts.route("/update")
-def update():
-    return "actualiza un contacto"
+@contacts.route("/update/<id>", methods=['GET' , 'POST'])
+def update(id):
+    contact = Contact.query.get(id)
+    if request.method == 'POST':
+        contact.fullname = request.form['fullname']
+        contact.phone = request.form['phone']
+
+        db.session.commit()
+        return redirect(url_for('contacts.index'))
+        
+    return render_template('update.html', update=contact)
 
 @contacts.route("/delete/<id>")
 def delete(id):
     del_contact = Contact.query.get(id)
     db.session.delete(del_contact)
     db.session.commit()
-    return "elimina un contacto"
+    return redirect(url_for('contacts.index'))
 
 # Exportar blueprint al archivo de configuracion app.py
